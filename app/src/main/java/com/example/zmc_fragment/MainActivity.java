678c5505b6e46deec3c07c5b0e78d6fa354b6295
Summerdise <350464517@qq.com>
2020-08-16 22:49:58 +0800

    [zmc] feat: add android button onclick showing

--- app/src/main/java/com/example/zmc_fragment/MainActivity.java
+++ app/src/main/java/com/example/zmc_fragment/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.zmc_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button androidButton = findViewById(R.id.button);
+        androidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                Fragment fragmentAndroid = new Fragment_android();
+                transaction.add(R.id.fragment_container,fragmentAndroid);
+                transaction.commit();
+            }
+        });
     }
+
+
+    private void addFragment(Fragment fragment, String tag) {
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        transaction.add(R.id.fragment_container, fragment, tag);
+
+        transaction.commit();
+
+    }
+
+
 }
