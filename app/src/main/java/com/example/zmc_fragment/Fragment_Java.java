b89897aaead42bd8634f8de98be498e972d7b1bf
Summerdise <350464517@qq.com>
2020-08-16 21:51:01 +0800

    [zmc] feat: add fragment_android layout

--- app/src/main/java/com/example/zmc_fragment/Fragment_Java.java
+++ app/src/main/java/com/example/zmc_fragment/Fragment_Java.java
@@ -59,6 +59,6 @@ public class Fragment_Java extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__java, container, false);
+        return inflater.inflate(R.layout.fragment_java, container, false);
     }
 }
